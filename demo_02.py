70620016d14684610bd3d97d915e753f8aa210e3
Naohiro2g <naohiro2g@code2create.club>
2020-11-13 16:06:32 +0900

    update

--- demo_02.py
+++ demo_02.py
@@ -1,8 +1,8 @@
 # demo for 7-segment simulation
+# using the class 'Seven_seg' in seven_seg_pg.py
 
 import pygame
 from pygame.locals import Rect
-
 from seven_seg_pg import Seven_seg
 
 
@@ -10,31 +10,32 @@ DARK_GRAY = (40, 40, 40)
 GRAY = (80, 80, 80)
 RED = (255, 0, 0)
 GREEN = (10, 250, 10)
+CYAN = (120, 120, 250)
 YELLOW = (250, 250, 20)
 WHITE = (250, 250, 250)
 
 pygame.init()
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode([320, 320])
+screen = pygame.display.set_mode([400, 320])
 pygame.display.set_caption("pygame 7-segment display simulation")
 screen.fill(DARK_GRAY)
 
 display1 = Seven_seg(screen)
 display1.init_col(BLOCK_SIZE=6, BLOCK_INTV=8, COLOR_ON=GREEN, COLOR_OFF=GRAY)
-display1.init_line(X_ORG=2, Y_ORG=8, X_INTV=6)
+display1.init_row(X_ORG=8, Y_ORG=8, COL_INTV=6)
 
 display2 = Seven_seg(screen)
-display2.init_col(BLOCK_SIZE=3, BLOCK_INTV=4, COLOR_ON=RED, COLOR_OFF=GRAY)
-display2.init_line(X_ORG=14, Y_ORG=30, X_INTV=6)
+display2.init_col(BLOCK_SIZE=6, BLOCK_INTV=8, COLOR_ON=CYAN, COLOR_OFF=GRAY)
+display2.init_row(X_ORG=2, Y_ORG=16, COL_INTV=6)
 
 display3 = Seven_seg(screen)
-display3.init_col(BLOCK_SIZE=3, BLOCK_INTV=4, COLOR_ON=YELLOW, COLOR_OFF=GRAY)
-display3.init_line(X_ORG=8, Y_ORG=40, X_INTV=6)
+display3.init_col()
+display3.init_row(X_ORG=20, Y_ORG=60, COL_INTV=6)
 
 display4 = Seven_seg(screen)
-display4.init_col(BLOCK_SIZE=6, BLOCK_INTV=8, COLOR_ON=WHITE, COLOR_OFF=GRAY)
-display4.init_line(X_ORG=2, Y_ORG=30, X_INTV=8)
+display4.init_col()
+display4.init_row(X_ORG=2, Y_ORG=70, COL_INTV=6)
 
 
 running = True
@@ -49,26 +50,25 @@ while running:
             break
         # 「for count」のループから抜ける。whileループも抜ける。
 
-        display1.update(col=0, num=count // (16 ** 3)) # 4096の位
-        display1.update(col=1, num=count // (16 ** 2)) # 256の位
-        display1.update(col=2, num=count // 16)  # 16の位
-        display1.update(col=3, num=count % 16)   # 1の位
+        display1.update_col(col=0, num=count // (16 ** 3)) # 4096の位
+        display1.update_col(col=1, num=count // (16 ** 2)) # 256の位
+        display1.update_col(col=2, num=count // 16)  # 16の位
+        display1.update_col(col=3, num=count)   # 1の位
+    
+        display2.update_col(col=0, num=count // (10 ** 4), base=10) # 1000の位
+        display2.update_col(col=1, num=count // (10 ** 3), base=10) # 1000の位
+        display2.update_col(col=2, num=count // (10 ** 2), base=10) # 100の位
+        display2.update_col(col=3, num=count // (10 ** 1), base=10) # 10の位
+        display2.update_col(col=4, num=count // (10 ** 0), base=10) # 1の位
 
-        display2.update(col=0, num=count // (16 ** 3))  # 4096の位
-        display2.update(col=1, num=count // (16 ** 2))  # 256の位
+        display3.disp_num2(zfil=False, rjust=3, num=count, base=10)
 
-        display3.update(col=0, num=(count // (16 ** 2)) // 100, base=10)
-        display3.update(col=1, num=(count // (16 ** 2)) // 10, base=10)
-        display3.update(col=2, num=(count // (16 ** 2)), base=10)
+        display4.disp_num2(zfil=True, rjust=16, num=count, base=2)
 
-        display4.update(col=0, num=0, base=10)
-        display4.update(col=1, num=1, base=10)
-        display4.update(col=2, num=2, base=10)
-        display4.update(col=3, num=12)
-        display4.update(col=4, num=15, base=16)
 
-        pygame.display.flip()  # update
-        clock.tick(120)  # FPS, Frame Per Second
+        pygame.display.flip()  # update_col
+        clock.tick(20)  # FPS, Frame Per Second
+    screen.fill(DARK_GRAY)
 # infinit loop bottom ----
 
 pygame.quit()
--- seven_seg_pg.py
+++ seven_seg_pg.py
@@ -2,6 +2,7 @@
 # 各セグメントをブロック２個で構成。
 # 4x7ドットマトリクスになっている。
 
+from math import log
 import pygame
 from pygame.locals import Rect
 
@@ -31,6 +32,7 @@ offset_G = ((1,3), (2,3))
 # 表示する数字／記号のon/off情報
 # セグメントAからGについて7種類、「タプル」で用意する。
 # 加えて、各セグメントのオフセット情報を16番目の要素、セグメント名を17番目の要素として持つ。
+# セグメント名は本来の動作には不要。
 seg_A = (1, 0, 1, 1,   0, 1, 1, 1,   1, 1, 1, 0,   1, 0, 1, 1,  offset_A, "A")
 seg_B = (1, 1, 1, 1,   1, 0, 0, 1,   1, 1, 1, 0,   0, 1, 0, 0,  offset_B, "B")
 seg_C = (1, 1, 0, 1,   1, 1, 1, 1,   1, 1, 1, 1,   0, 1, 0, 0,  offset_C, "C")
@@ -45,39 +47,51 @@ seg_G = (0, 0, 1, 1,   1, 1, 1, 0,   1, 1, 1, 1,   0, 1, 1, 1,  offset_G, "G")
 # さらに、タプルのタプルを作る。
 segments = (seg_A, seg_B, seg_C, seg_D, seg_E, seg_F, seg_G)
 
+# 色の準備。R, G, Bを0から255で指定。
+DARK_GRAY = (40, 40, 40)
+GRAY = (80, 80, 80)
+RED = (255, 0, 0)
+GREEN = (10, 250, 10)
+YELLOW = (250, 250, 20)
+WHITE = (250, 250, 250)
+
 
 class Seven_seg():
     def __init__(self, screen):
         self.screen = screen    
 
-    def init_col(self, BLOCK_SIZE=12, BLOCK_INTV=16, COLOR_ON=(250, 10, 10), COLOR_OFF=(80, 80, 80)):
-        # １桁、コラムの設定
+    def init_col(self, BLOCK_SIZE=3, BLOCK_INTV=4, COLOR_ON=WHITE, COLOR_OFF=GRAY):
+        # ひと桁、コラムの設定
         # 7セグをセグメントあたり2個のブロックで構成
-        # そのサイズと配置間隔、ピクセル指定
+        # ブロックのサイズと配置間隔をピクセル指定（インターバル）
         self.BLOCK_SIZE = BLOCK_SIZE
         self.BLOCK_INTV = BLOCK_INTV
         # on/offのカラー
         self.COLOR_ON = COLOR_ON
         self.COLOR_OFF = COLOR_OFF
 
-    def init_line(self, X_ORG=2, Y_ORG=8, X_INTV=6):  # 表示ラインの設定
-        # xy空間での7セグ表示、最上位桁の左下座標、ブロック数指定
+    def init_row(self, X_ORG=2, Y_ORG=8, COL_INTV=6):  # 表示行の設定
+        # xy空間での7セグ表示、最上位桁の左下座標をブロック数で指定
         self.X_ORG = X_ORG * self.BLOCK_INTV
         self.Y_ORG = Y_ORG * self.BLOCK_INTV
-        # 各桁のブロック間隔、ブロック数指定
-        self.X_INTV = X_INTV * self.BLOCK_INTV 
+        # 各桁のブロック間隔をブロック数で指定（インターバル）
+        self.COL_INTV = COL_INTV * self.BLOCK_INTV 
 
-    def update(self, col=0, num=3, base=16):  # ある桁にある数字を表示する関数
-        # numをcol桁目にブロック表示、桁は左から右へ進む。
+    def update_col(self, col=0, num=3, base=16, blank=False):  # ある桁にある数字を表示する関数
+        # numをcol桁目に表示、桁は最上位桁の左から右へ進む。
         block_size = self.BLOCK_SIZE
         num = num % base  # デフォルトは16進数表示
-        for segment in segments:  # 7つのセグメントのうちの１セグメントについて、
+        for segment in segments:  # 7つのセグメントのうちの、あるセグメントについて、
             if segment[num] == 1:
                 color = self.COLOR_ON
             else:
                 color = self.COLOR_OFF
+            if blank is True:    # ブランク表示の場合は、すべてOFFで上書き
+                color = self.COLOR_OFF
             # 桁の原点
-            x0 = self.X_ORG + self.X_INTV * col
+            # if col < 0:
+            #     col = 0
+            x0 = self.X_ORG + self.COL_INTV * col
             y0 = self.Y_ORG
             # ブロック1、ブロック2の座標オフセット
             x1, y1 = segment[16][0]
@@ -88,3 +102,29 @@ class Seven_seg():
             # ブロック１，２を描く
             pygame.draw.rect(self.screen, color, Rect(org1[0], org1[1], block_size, block_size))
             pygame.draw.rect(self.screen, color, Rect(org2[0], org2[1], block_size, block_size))
+
+    def disp_num(self, num=1234, base=16):
+        # numを複数桁で表示する。左詰め。
+        if num <= 0:
+            num = 1
+        num_cols = int(log(num, base)) + 1
+        for col in range(num_cols):
+            self.update_col(col=col, num=num // (base ** (num_cols - col - 1)), base=base)
+
+    def disp_num2(self, rjust=4, zfil=False, num=1234, base=16):
+        # numをrjust桁で右詰め表示する。桁あふれが起きると、右にずれていく。
+        # zfil==Trueの時、上位桁をゼロで埋める。Falseの場合は、ブランク表示。
+        if num <= 0:
+            num = 1
+        num_cols = int(log(num, base)) + 1
+        if num_cols > rjust:
+            rjust = num_cols
+        for disp_col in range(rjust):
+            col = disp_col + num_cols - rjust
+            if col >= 0:
+                self.update_col(col=disp_col, num=num // (base ** (num_cols - col - 1)), base=base)
+            else:
+                if zfil is True:
+                    self.update_col(col=disp_col, num=0)
+                else:
+                    self.update_col(col=disp_col, blank=True)
