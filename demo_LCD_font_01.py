bff65fddbc1533063650667add6a46d2515fcd39
yu184743 <96573503+yu184743@users.noreply.github.com>
2024-07-11 20:51:39 +0900

    demo_LCD_font_.py add 3 ~8

--- demo_LCD_font_01.py
+++ demo_LCD_font_01.py
@@ -31,7 +31,7 @@ lcd1.init_row(X_ORG=8, Y_ORG=8, COL_INTV=6)
 
 
 def LCD_display(x, y):
-    code = int((x / 8) % 3)
+    code = int((x / 8) % 10)
     text1, rect1 = font1.render(str(code), WHITE)
     rect1.center = (x, y)
     screen.blit(text1, rect1)
