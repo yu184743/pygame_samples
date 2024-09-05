0e4efe48305d9327f8f47f2583e7d98319775640
yu184743 <96573503+yu184743@users.noreply.github.com>
2024-09-05 21:07:41 +0900

    add lcd_font_mc

--- demo_05.py
+++ demo_05.py
@@ -1,7 +1,84 @@
-from mcje.minecraft import Minecraft
-import param_MCJE1122 as param
+# demo for 7-segment simulation
+# using the class 'Seven_seg' in seven_seg_pg.py
 
-mc = Minecraft.create(port=param.PORT_MC)
-mc.postToChat('Hello world')
+from datetime import datetime
+import pygame
+from seven_seg_pg import Seven_seg
+from lcd_font_mc import LCD_font
 
-mc.setBlock(0, 6, 0, param.DIAMOND_BLOCK)
+
+DARK_GRAY = (40, 40, 40)
+GRAY = (80, 80, 80)
+RED = (255, 0, 0)
+GREEN = (10, 250, 10)
+CYAN = (120, 120, 250)
+YELLOW = (250, 250, 20)
+WHITE = (250, 250, 250)
+
+pygame.init()
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode([800, 640])
+pygame.display.set_caption("pygame 7-segment display simulation")
+screen.fill(DARK_GRAY)
+
+
+
+
+display2 = LCD_font(screen)
+display2.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=RED, COLOR_OFF=GRAY)
+display2.init_row(X_ORG=2, Y_ORG=18, COL_INTV=6)
+
+displaytime = LCD_font(screen)
+displaytime.init_col(BLOCK_SIZE=6, BLOCK_INTV=8, COLOR_ON=CYAN, COLOR_OFF=GRAY)
+displaytime.init_row(X_ORG=2, Y_ORG=8, COL_INTV=6)
+
+displaydate = LCD_font(screen)
+displaydate.init_col(BLOCK_SIZE=6, BLOCK_INTV=8, COLOR_ON=WHITE, COLOR_OFF=GRAY)
+displaydate.init_row(X_ORG=2, Y_ORG=28, COL_INTV=6)
+
+
+
+running = True
+# infinite loop top ----
+while running:
+    for count in range(16 ** 4):  # 0から65535まで
+        # press ctrl-c or close the window to stop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        if not running:
+            break
+        # 「for count」のループから抜ける。whileループも抜ける。
+
+        dt_now = datetime.now()
+        time_now = (dt_now.hour * 10000
+                    + dt_now.minute * 100
+                    + dt_now.second)
+        
+        displaytime.update_col(col=0, code=dt_now.hour // 10)
+        displaytime.update_col(col=1, code=dt_now.hour % 10)
+        displaytime.update_col(col=2, code=11)
+        displaytime.update_col(col=3, code=dt_now.minute // 10)
+        displaytime.update_col(col=4, code=dt_now.minute % 10)
+        displaytime.update_col(col=5, code=11)
+        displaytime.update_col(col=6, code=dt_now.second // 10)
+        displaytime.update_col(col=7, code=dt_now.second %10)
+
+        displaydate.update_col(col=0, code=int(str(dt_now.year)[0])) 
+        displaydate.update_col(col=1, code=int(str(dt_now.year)[1]))
+        displaydate.update_col(col=2, code=int(str(dt_now.year)[2]))   
+        displaydate.update_col(col=3, code=int(str(dt_now.year)[3]))    
+        displaydate.update_col(col=4, code=11)
+        displaydate.update_col(col=5, code=dt_now.month // 10)
+        displaydate.update_col(col=6, code=dt_now.month % 10)
+        displaydate.update_col(col=7, code=11) 
+        displaydate.update_col(col=8, code=dt_now.day // 10)
+        displaydate.update_col(col=9, code=dt_now.day % 10)  
+
+        pygame.display.flip()  # update_col
+        clock.tick(20)  # FPS, Frame Per Second
+    # screen.fill(DARK_GRAY)
+# infinit loop bottom ----
+
+pygame.quit()
---
+++ lcd_font_mc.py
@@ -0,0 +1,142 @@
+# handmade LCD font for pygame
+# 5x7ドットマトリクス
+
+# from math import log
+import pygame
+from pygame.locals import Rect
+from mcje.minecraft import Minecraft
+import param_MCJE1122 as param
+mc = Minecraft.create(port=param.PORT_MC)
+mc.postToChat('create clock')
+
+LCD_0 = (0, 0, 1, 1, 0,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 0, 1, 1, 0)
+
+LCD_1 = (0, 0, 1, 0, 0,
+         0, 1, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 1, 1, 1, 0)
+
+LCD_2 = (0, 0, 1, 1, 0,
+         0, 1, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0,
+         0, 1, 0, 0, 0,
+         0, 1, 1, 1, 1)
+
+LCD_3 = (0, 0, 1, 1, 0,
+         0, 1, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 1, 1, 0,
+         0, 0, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 0, 1, 1, 0)
+
+LCD_4 = (0, 0, 0, 1, 0,
+         0, 0, 1, 1, 0,
+         0, 0, 1, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 0, 1, 0,
+         0, 1, 1, 1, 1,
+         0, 0, 0, 1, 0)
+
+LCD_5 = (0, 1, 1, 1, 1,
+         0, 1, 0, 0, 0,
+         0, 1, 1, 1, 0,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 0, 1, 1, 0)
+
+LCD_6 = (0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0,
+         0, 1, 0, 0, 0,
+         0, 1, 1, 1, 0,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 0, 1, 1, 0)
+
+LCD_7 = (0, 1, 1, 1, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 1, 0,
+         0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0)
+         
+LCD_8 = (0, 0, 1, 1, 0,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 0, 1, 1, 0,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 0, 1, 1, 0)
+
+LCD_9 = (0, 0, 1, 1, 0,
+         0, 1, 0, 0, 1,
+         0, 1, 0, 0, 1,
+         0, 0, 1, 1, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0)
+
+# フォントのよみこみ
+with open("fonts/4x7_number.txt",encoding="utf-8") as f:
+    LCD_font_styles = (f.read().split('\n'))
+
+DARK_GRAY = (40, 40, 40)
+GRAY = (80, 80, 80)
+RED = (255, 0, 0)
+GREEN = (10, 250, 10)
+YELLOW = (250, 250, 20)
+WHITE = (250, 250, 250)
+
+
+class LCD_font():
+    def __init__(self, mc):
+        self.mc = mc
+
+    def init_col(self, BLOCK_SIZE=4, BLOCK_INTV=4, COLOR_ON=WHITE, COLOR_OFF=GRAY):
+        # ひと桁、コラムの設定
+        # ブロックのサイズと配置間隔をピクセル指定（インターバル）
+        self.BLOCK_SIZE = BLOCK_SIZE
+        self.BLOCK_INTV = BLOCK_INTV
+        # on/offのカラー
+        self.COLOR_ON = COLOR_ON
+        self.COLOR_OFF = COLOR_OFF
+
+    def init_row(self, X_ORG=2, Y_ORG=8, COL_INTV=6):  # 表示行の設定
+        # xy空間での7セグ表示、最上位桁の左下座標をブロック数で指定
+        self.X_ORG = X_ORG * self.BLOCK_INTV
+        self.Y_ORG = Y_ORG * self.BLOCK_INTV
+        # 各桁のブロック間隔をブロック数で指定（インターバル）
+        self.COL_INTV = COL_INTV * self.BLOCK_INTV
+
+    def update_col(self, col=0, code=2):  # ある桁にある文字を表示する関数
+        # codeの文字をcol桁目に表示、桁は最上位桁の左から右へ進む。
+        block_size = self.BLOCK_SIZE
+        i = 0
+        for y in range(7):
+            for x in range(5):
+                if LCD_font_styles[code *7 + y][x] == "1":
+                    color = self.COLOR_ON
+                else:
+                    color = self.COLOR_OFF
+                # 桁の原点
+                x0 = self.X_ORG + self.COL_INTV * col
+                y0 = self.Y_ORG
+                # ドットの原点座標
+                org1 = (x0 + x * self.BLOCK_INTV, y0 + y * self.BLOCK_INTV)
+                # ドットを描く
+                mc.setBlock(5, 5, 5, param.DIAMOND_BLOCK) # z=定数
+                i += 1
