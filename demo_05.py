14103202d548aff272d6650ee9eff634ac693bf9
yu184743 <96573503+yu184743@users.noreply.github.com>
2024-09-05 20:14:42 +0900

    demo_05 mc.setblock

--- demo_05.py
+++ demo_05.py
@@ -1,2 +1,7 @@
-
+from mcje.minecraft import Minecraft
 import param_MCJE1122 as param
+
+mc = Minecraft.create(port=param.PORT_MC)
+mc.postToChat('Hello world')
+
+mc.setBlock(0, 6, 0, param.DIAMOND_BLOCK)
